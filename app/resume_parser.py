3d417c9676e55f94e4c94a0b8f549679acb2fe4c
Harshita Anala <100120978+harshitaanala@users.noreply.github.com>
2025-06-03 22:56:47 +0530

    Update resume_parser.py

--- app/resume_parser.py
+++ app/resume_parser.py
@@ -1,7 +1,8 @@
-import fitz  # PyMuPDF
+import fitz  # from pymupdf
 
 def extract_text_from_pdf(file):
-    doc = fitz.open(stream=file.read(), filetype="pdf")
+    file_bytes = file.read()
+    doc = fitz.open(stream=file_bytes, filetype="pdf")
     text = ""
     for page in doc:
         text += page.get_text()
